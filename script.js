9d76f739278230a3d65312e1871e6ecaf97fa168
AayushiThakare <99353260+AayushiThakare@users.noreply.github.com>
2022-07-23 00:06:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function add() {
+    document.getElementById('input')
+    alert("Product is added in cart")
+    console.log("Product is Added");
+    
+    
+}
+function remove() {
+    document.getElementById('rev')
+    console.log("Delet the Product");
+    alert("Product is remove from cart");
+    
+    
+}
+    
